5a6dca14a482cc5decb2f32c1da0a3a0b225ab1b
otaviopacheco <aapo5719@gmail.com>
2023-10-11 21:11:38 -0300

    firts Script and commit

---
+++ auladev.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class auladev : MonoBehaviour
+{
+    int numero; // variaveis devem ser inicializadas fora dos metodos
+    GameObject go;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("teste");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
